ba5b97ab3d0dec3131e23d020ad124262566d6fb
sangmin Moon <msmasd@naver.com>
2019-08-01 22:16:04 +0900

    sorting 소스 정리(selectionSort먼저구현)

---
+++ eclipse/SelectionSort/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+//		System.out.println("hello cmd java");
+//		String[] a = readAllStrings();
+		String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+		Selection.sort(a);
+		assert Selection.isSorted(a);
+		Selection.show(a);
+	}
+	
+	private static String[] readAllStrings() {
+		Scanner scan = new Scanner(System.in);
+		ArrayList<String> result = new ArrayList<String>();
+		while(scan.hasNext()) {
+			result.add(scan.next());
+		}
+		return result.toArray(new String[result.size()]);
+	}
+
+}
