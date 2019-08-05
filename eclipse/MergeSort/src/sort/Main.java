7b27ec44595e1777e8cb6803ea1f2b1519cee888
sangmin Moon <msmasd@naver.com>
2019-08-05 21:04:26 +0900

    퀵정렬 구현부

--- eclipse/MergeSort/src/sort/Main.java
+++ eclipse/MergeSort/src/sort/Main.java
@@ -3,7 +3,11 @@ package sort;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello mergeSort");
+		String[] quickInput = {"K", "R", "A", "T", "E", "L", "E", "P", "U", "I", "M", "Q", "C", "X", "O", "S"};
+		Quick.sort(quickInput);
+		for(String item : quickInput) {
+			System.out.print(item + " ");				
+		}
 	}
 
 }
